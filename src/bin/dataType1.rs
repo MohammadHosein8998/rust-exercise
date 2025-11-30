5f3d1e6241bce10d2d06e6b01cca5c57002ef366
mohammadHosain <mohammad.hosain8998@gmail.com>
2025-12-01 00:54:52 +0330

    adding variable9 to the project about arrays data type

---
+++ src/bin/dataType1.rs
@@ -0,0 +1,16 @@
+fn main(){
+    // Integer
+    let sixtee_bit_signed = -32500;
+    let sixtee_bit_unsigned = 6400;
+
+
+    let thirty_two_signed = -212516545;
+    let thirty_two_signed: u32 = 4291545564;
+
+    let som_value = 20u16;
+
+    // visual separator for number 
+
+    let big_number: i64 = 54_646_484_646_411;
+
+}
---
+++ src/bin/variables9.rs
@@ -0,0 +1,9 @@
+
+fn  main(){
+    let numbers = [0,1,2,3,4,5,6,7,8,9];
+    let apples = ["hello" , "asd" , "2.31"];
+    println!("Length : {}" , apples.len());
+    println!("numbers array : {numbers:#?}" , );
+
+    dbg!(apples);
+}
